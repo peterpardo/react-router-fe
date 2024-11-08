a7ef4bb78fc139647f2cd888f81bade8ee034e4c
Peter Pardo <peterpardo123@gmail.com>
2024-11-08 21:44:07 +0800

    setup basic routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./routes/HomePage";
+import ErrorPage from "./routes/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/public",
+    element: <div>Public page</div>,
+  },
+  {
+    path: "/protected",
+    element: <div>Protected Page</div>,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
